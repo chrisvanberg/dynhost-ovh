a3ae7299eb5ab258306b7577f8b1f4c033f1f6ca
Chris Vanberg <contact@chrisvanberg.com>
2022-03-06 22:20:15 +0100

    better security

--- dynhost.sh
+++ dynhost.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-# Account configuration
-HOST=DOMAINE_NAME
-LOGIN=LOGIN
-PASSWORD=PASSWORD
 
 PATH_LOG=/var/log/dynhostovh.log
 
@@ -23,7 +19,7 @@ then
 else
   if [ "$HOST_IP" != "$CURRENT_IP" ]
   then
-    RES=$(curl -m 5 -L --location-trusted --user "$LOGIN:$PASSWORD" "https://www.ovh.com/nic/update?system=dyndns&hostname=$HOST&myip=$CURRENT_IP")
+    RES=$(curl -m 5 -L --user "$LOGIN:$PASSWORD" "https://www.ovh.com/nic/update?system=dyndns&hostname=$HOST&myip=$CURRENT_IP") 
     echo "[$CURRENT_DATETIME]: IPv4 has changed - request to OVH DynHost: $RES" >> $PATH_LOG
   fi
 fi
